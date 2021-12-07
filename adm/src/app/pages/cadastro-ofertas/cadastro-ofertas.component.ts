14629f838ee4fd29b7ddf83e5050eeab793e12bb
ivopozzani <ivo.pozzani@gmail.com>
2021-12-07 17:02:05 -0300

    Validacoes formulario e botao salvar
    Foram aplicadas as seguintes validacoes nos campos de preco: Obrigatorio, maior que zero e precoDesconto maior que preco
    Botao "salvar" agora chama (onSubmit) no formulario

--- adm/src/app/pages/cadastro-ofertas/cadastro-ofertas.component.ts
+++ adm/src/app/pages/cadastro-ofertas/cadastro-ofertas.component.ts
@@ -8,7 +8,6 @@ import {
   Validators
 } from '@angular/forms'
 import { Oferta } from 'src/app/oferta'
-
 import { CadastroService } from './cadastro.service'
 
 @Component({
@@ -23,26 +22,73 @@ export class CadastroOfertasComponent implements OnInit {
     { id: 3, nome: 'Steam' }
   ]
 
-  cadastroForm = new FormGroup({
-    id: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^[0-9]*$')
-    ]),
-    titulo: new FormControl('', Validators.required),
-    preco: new FormControl('', Validators.required),
-    precoDesconto: new FormControl('', Validators.required)
-  })
-
   constructor(private cadastroservice: CadastroService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 
-  novaOferta() {
-    this.cadastroservice.dataSource2.push(this.cadastroForm.value)
-    console.log(this.cadastroservice.dataSource2)
+  cadastroForm = new FormGroup({
+    id: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[0-9]+$/)
+    ]),
+    titulo: new FormControl(null, Validators.required),
+    preco: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[0-9]+\.[0-9][0-9]$/),
+      biggerThan('0.00')
+    ]),
+    precoDesconto: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[0-9]+\.[0-9][0-9]$/),
+      biggerThan('0.00'),
+      isSmaller('preco')
+    ])
+  })
+
+  onSubmit() {
+    console.log(this.cadastroForm)
+    //this.cadastroservice.dataSource2.push(this.cadastroForm.value)
+    //this.cadastroForm.reset()
   }
 
   get editaOferta() {
     return this.cadastroservice.getOferta()
   }
+}
+
+// Função que valida o preço maior que zero
+export function biggerThan(preco: string): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    if (parseFloat(control.value) <= parseFloat(preco)) {
+      return { biggerThan: { value: control.value } }
+    } else {
+      return null
+    }
+  }
+}
+
+// Função que valida o preçoDesconto menor que o preço normal
+export function isSmaller(preco: string) {
+  const validator = (formControl: FormControl) => {
+    if (preco == null) {
+      throw new Error('É necessário informar o preço')
+    }
+
+    if (!formControl.root || !(<FormGroup>formControl.root).controls) {
+      return null
+    }
+
+    const field = (<FormGroup>formControl.root).get(preco)
+
+    if (!field) {
+      throw new Error('É necessário informar um campo de formulário válido')
+    }
+
+    if (parseFloat(field.value) <= parseFloat(formControl.value)) {
+      return { isSmaller: preco }
+    }
+
+    return null
+  }
+  return validator
 }
